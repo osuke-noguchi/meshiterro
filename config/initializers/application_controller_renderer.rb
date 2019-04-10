3fa5dde06c48e18a4986929b081db1b44711c847
野口央介 <noguchihiroshisuke@noguchihiroshisukenoMacBook-Pro.local>
2019-04-10 19:41:01 +0900

    2章：meshiterro以下

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '3b221e5c780b6349d40b387a01ad08d52b2e0cf1381ab4e642abb2c790294007dec94c9c74171235d42bcc3e3e251963c11e9d3cae599eafa46a5244c3faae2f'
